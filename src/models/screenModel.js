7c24db63e1b96b191e08edd07b91e1a6fff3f156
sandeshpawar10 <spawar3699@gmail.com>
2026-04-12 12:32:41 +0530

    modified screen model

--- src/models/screenModel.js
+++ src/models/screenModel.js
@@ -2,21 +2,46 @@ const mongoose = require("mongoose")
 const {Schema} = require("mongoose")
 
 const screenSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
     theatre: {
         type: Schema.Types.ObjectId,
-        ref: "theatre"
+        ref: "theatre",
+        required: true
     },
-    screenNumber: Number,
-    totalSeats: Number,
+    screenNumber: {
+        type: Number,
+        required: true
+    },
+    totalSeats: {
+        type: Number,
+        required: true
+    },
     seatLayout: [
         {
-            seatNumber: String,
-            type:{
+            row:{
                 type: String,
-                enum: ["silver","gold","platinum"]
+                required: true
+            },
+            seats:{
+                seatNumber: Number,
+                type:{
+                    type: String,
+                    enum: ["silver","gold","platinum"]
+                },
+                isBooked: {
+                    type: Boolean,
+                    default: false
+                }
             }
         }
-    ]
+    ],
+    isActive: {
+        type: Boolean,
+        default: true
+    }
 })
 
 const screen = mongoose.model("screen",screenSchema)
