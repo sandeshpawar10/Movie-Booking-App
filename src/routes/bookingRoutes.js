3ac33212e6fbf5ffbdab9296b12e4fca42a0c286
sandeshpawar10 <spawar3699@gmail.com>
2026-04-15 23:29:07 +0530

    booking routes added

---
+++ src/routes/bookingRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router = express.Router()
+const controller = require("../controllers/bookingController")
+const {verifyJWt} = require("../middlewares/authenticationMiddleware")
+const {checkAdmin} = require("../middlewares/roleMiddleware")
+
+router.post('/booking/create-booking',verifyJWt,controller.createBooking)
+router.post('/booking/confirm-booking/:bookingId',verifyJWt,controller.confirmBooking)
+router.delete('/booking/cancel-booking/:bookingid',verifyJWt,controller.cancelBooking)
+router.get('/booking/booking-by-id/:id',verifyJWt,controller.getBookingById)
+router.get('/booking/booking-by-show/:showid',verifyJWt,checkAdmin,controller.getBookingByShowId)
+router.get('/booking/all-bookings',verifyJWt,checkAdmin,controller.getAllBookings)
+router.get('/booking/my-bookings/:userid',verifyJWt,controller.getBookingByUser)
+
+
+module.exports = router
